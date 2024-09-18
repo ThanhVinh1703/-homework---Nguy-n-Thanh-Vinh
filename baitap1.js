2034173f85ddbaf988033ab296a054821f197ad4
ThanhVinh1703 <nguyenthanhvinh170398@gmail.com>
2024-09-18 14:36:11 +0700

    bài tập về nhà

---
+++ baitap1.js
@@ -0,0 +1,59 @@
+// bài tập 1
+// alert("xin chào");
+
+
+// bài tap 2
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = "Hà Nội";
+
+// console.log(1);
+// console.log(f);
+// console.log(b);
+// console.log(s);
+
+
+
+// bai tap 3
+// let width = +prompt("enter the width");
+// let height = +prompt("enter the height");
+// let area =  width * height;
+// console.log("area "  + "is " + area);
+
+
+// bai tap 4
+// confirm("bạn đã đủ tuổi chưa?")
+
+
+// bai tap 5
+// let physic = +prompt ("enter your physic score");
+// let chemistry = +prompt ("enter your chemistry   score");
+// let biology = +prompt ("enter your biology score");
+// let sum = physic + chemistry + biology;
+// let avg = (physic + chemistry + biology)/3;
+// console.log("Your total score for 3 subjects is "+ sum );
+// console.log("your average score of 3 subjects is "+ avg.toFixed(2));
+
+
+// bai tap 6
+// let CDegrees = +prompt("enter the Celsius to Fahrenheit");
+// let FDegrees = (CDegrees*9) /5 + 32;
+// console.log(CDegrees + "C = " + FDegrees + "F"); 
+
+
+// bai tap 7
+// let width = +prompt("Enter the width");
+// let length = +prompt("Enter the length");
+// let area = width * length;
+// let perimeter = (width + length) / 2;
+// console.log("your area is " + area);
+// console.log("your perimeter is " + perimeter);
+
+// bai tap 8
+// const PI = Math.PI;
+// let Radius = +prompt("Enter your radius:");
+// let area = Radius * Radius * PI;
+// let perimeter = 2 * Radius * PI;
+// console.log("your area is "+ area.toFixed(2));
+// console.log("your peremeter is "+ perimeter.toFixed(2));
